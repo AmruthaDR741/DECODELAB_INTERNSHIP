59d54d143dd818cd6c076e37708a72dc9523eb6b
amruthadr741@gmail.com <amruthadr741@gmail.com>
2026-02-28 20:23:12 +0530

    First commit

---
+++ decodelab2.py
@@ -0,0 +1,23 @@
+# Expense Tracker - Project 2
+
+print("Welcome to Expense Tracker")
+print("Enter your expenses one by one.")
+print("Type 'done' to finish.\n")
+
+total = 0  # accumulator variable
+
+while True:
+    user_input = input("Enter expense amount: ")
+
+    if user_input.lower() == 'done':
+        break
+
+    try:
+        expense = float(user_input)
+        total = total + expense   # accumulator logic
+        print("Added successfully!\n")
+    except ValueError:
+        print("Invalid input! Please enter a number.\n")
+
+print("\nTotal Spent =", total)
+print("Thank you!")
